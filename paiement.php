1b24eed972ca336d08692e92461ff9c9e6ede320
user648 <133900264+jean7mkl@users.noreply.github.com>
2025-03-31 09:39:40 +0000

    origin/main

--- paiement.php
+++ paiement.php
@@ -1,11 +1,12 @@
 <?php
+session_start();
 require('getapikey.php'); // Assurez-vous d'avoir téléchargé ce fichier
 
 // Données de la transaction
 $transaction = uniqid(); // Génère un identifiant unique pour chaque transaction
 $montant = "18000.99"; // Montant avec 2 chiffres après la virgule
-$vendeur = "MI-1_A"; // Remplacez par votre code vendeur
-$retour = "http://localhost/retour_paiement.php"; // URL de retour après le paiement
+$vendeur = "MEF-1_E"; // Remplacez par votre code vendeur
+$retour = "http://localhost:8080/jalon2/historique.php"; // URL de retour après le paiement
 
 // Récupération de l'API Key
 $api_key = getAPIKey($vendeur);
@@ -23,10 +24,37 @@ $control = md5($api_key . "#" . $transaction . "#" . $montant . "#" . $vendeur .
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
+    <link rel=stylesheet href="sitedevoyage.css">
     <title>Interface de Paiement</title>
 </head>
 <body>
-    <h2>Effectuer un Paiement</h2>
+<header>
+        <div class="header-container">
+            <div class="logo-title">
+                <a href="projet.php" class="logo-link">
+                    <img src="photo/Logo.png" alt="Logo Travel4all" class="logo">
+                </a>
+                <h1>WHERE2GO</h1>
+            </div>
+            <nav>
+                <a href="projet.php" class="btn-nav">Notre Accueil</a>
+                <a href="pagequizz.php" class="btn-nav">Découvrir notre concept</a>
+                <a href="présprojet.php" class="btn-nav">Qui sommes-nous?</a>
+            </nav>
+            <div class="header-auth">
+            <?php if (isset($_SESSION['login'])) : ?>
+                <!-- Bouton "Client" -->
+                <a href="moncompte.php" class="btn btn-primary"><?php echo htmlspecialchars($_SESSION["login"]); ?></a>
+                <a href="déconnexion.php" class="btn btn-primary">Se déconnecter</a>
+            <?php else : ?>
+                <!-- Boutons pour les non-connectés -->
+                <a href="connecter.php" class="btn btn-primary">Se connecter</a>
+                <p class="small-text">Nouveau client ? <a href="inscription.php">S'inscrire</a></p>
+            <?php endif; ?>
+        </div>
+        </div>
+    </header><center></br></br></br></br></br></br>
+    <h2 class="paiement">Effectuer un Paiement</h2>
     <form action="https://www.plateforme-smc.fr/cybank/index.php" method="POST">
         <input type="hidden" name="transaction" value="<?= $transaction ?>">
         <input type="hidden" name="montant" value="<?= $montant ?>">
@@ -34,6 +62,11 @@ $control = md5($api_key . "#" . $transaction . "#" . $montant . "#" . $vendeur .
         <input type="hidden" name="retour" value="<?= $retour ?>">
         <input type="hidden" name="control" value="<?= $control ?>">
         <input type="submit" value="Payer maintenant">
-    </form>
+    </form></center></br></br></br></br></br></br>
+    <footer>
+        <p>&copy; 2025 Agence de Voyage de Leo Bouabdallah, Thomas Ribeiro, Jean Moukarzel.<br> Tous droits réservés.</p> 
+    </footer>
+
+
 </body>
 </html>
